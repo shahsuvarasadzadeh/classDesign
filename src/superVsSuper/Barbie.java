d8bb0f2c3efc2d3085ee07dffa73672ef3c43477
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2022-11-23 16:24:00 +0400

    static ve default metodlar haqqinda mellumat

--- src/superVsSuper/Barbie.java
+++ src/superVsSuper/Barbie.java
@@ -1,6 +1,9 @@
 package superVsSuper;
 
 public class Barbie extends Doll{
+    Barbie(String nm) {
+        super(nm);
+    }
 //     String name; // line1
 //    Barbie(){
 //        this(name); // DOES NOT COMPILE
